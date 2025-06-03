bf59a318cd3690fe155abe9caf6d20215031c51b
omar203010 <omar.m.alanziy@gmail.com>
2025-06-03 19:16:28 +0300

    Initial project upload

--- test13/urls.py
+++ test13/urls.py
@@ -5,20 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('coreuploads.urls')),
+    path('', include('coreuploads.urls')),  # الصفحة الرئيسية ومجموعة روابط التطبيق
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-    from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    # باقي المسارات مثل: path('admin/', ...), إلخ
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
